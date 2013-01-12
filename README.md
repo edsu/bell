955f31ac26e05e37105acaeef6e5a308bbd5548f
Ed Summers <ehs@pobox.com>
2013-01-12 09:24:32 +0000

    initial commit with partial metadata

---
+++ README.md
@@ -0,0 +1,12 @@
+bell
+====
+
+A little script to scrape the [Alexander Graham Bell Family
+Papers](http://memory.loc.gov/ammem/bellhtml/bellhome.html) images at 
+the Library of Congress, for hopefully uploading to Wikisource for
+transcription. The scraper persists scraped data as JSON.
+
+License
+=======
+
+* Public Domain
---
+++ scrape.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import os
+import json
+import time
+import lxml.html
+import requests
+import urlparse
+
+seen = {}
+def load_html(url):
+    print "loading", url
+    if url in seen:
+        print "already seen %s %s times" % (url, seen[url])
+    seen[url] = seen.get(url, 0) + 1
+    time.sleep(1)
+    return lxml.html.fromstring(requests.get(url).content)
+
+def series_urls():
+    url = "http://memory.loc.gov/ammem/bellhtml/magbellSeries.html"
+    doc = load_html(url)
+    count = 1
+    for a in doc.xpath(".//a"):
+        if "bellhtml/" in a.attrib["href"]:
+            count += 1
+            yield a.text_content(), urlparse.urljoin(url, a.attrib["href"])
+            if count > 2:
+                pass #break
+
+def cgi_urls(url):
+    doc = load_html(url)
+    next_url = None
+    count = 0
+    for a in doc.xpath(".//a"):
+        link_text = a.text_content()
+        link_url = urlparse.urljoin(url, a.attrib["href"])
+        if "cgi-bin/query" in link_url:
+            if link_text == "PREV PAGE":
+                continue
+            elif link_text == "NEXT PAGE":
+                next_url = link_url
+            else:
+                count += 1
+                yield link_text, link_url
+            if count > 2:
+                pass #break
+    if next_url:
+        for t, u in cgi_urls(next_url):
+            yield t, u
+
+def img_urls(url):
+    doc = load_html(url)
+    next_url = None
+    archival_img = None
+    for a in doc.xpath(".//a"):
+        link_text = a.text_content()
+        link_url = urlparse.urljoin(url, a.attrib.get("href", None))
+        if "Archival" in link_text and "JPEG" in link_text:
+            archival_img = link_url
+        elif link_text == "NEXT IMAGE":
+            next_url = link_url
+
+    if archival_img:
+        yield archival_img
+
+    if next_url:
+        for u in img_urls(next_url):
+            yield u
+
+def transcription(img_url):
+    item_dir = os.path.dirname(item["images"][0])
+    item_id = os.path.basename(item_dir)
+    url = item_dir + "/" + item_id + ".xml"
+    if requests.head(url).status_code == 200:
+        print url
+        return url
+    return None
+ 
+collection = {"title": "Alexander Graham Bell Family Papers", "items": []}
+for series_text, series_url in series_urls():
+    series = {"title": series_text, "url": series_url, "items": []}
+    for subseries_text, subseries_url in cgi_urls(series_url):
+        subseries = {"title": subseries_text, "url": subseries_url, "items": []}
+        for item_text, item_url in cgi_urls(subseries_url):
+            item = {"title": item_text, "url": item_url, "images": []}
+            for img_url in img_urls(item_url):
+                print img_url
+                item["images"].append(img_url)
+            if len(item["images"]) == 0:
+                print "no images", item_url
+            item["transcription"] = transcription(item["images"][0])
+            subseries["items"].append(item)
+        series["items"].append(subseries)
+    collection["items"].append(series)
+
+open("metadata.json", "w").write(json.dumps(collection, indent=2))
